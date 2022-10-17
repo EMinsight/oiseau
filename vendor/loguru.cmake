dc1866b91626e0e8e5a4d1c1f41482e4b48ea625
tiagovla <tiagovla@gmail.com>
2022-10-17 05:31:06 -0300

    feat: add logging vendor

---
+++ vendor/loguru.cmake
@@ -0,0 +1,13 @@
+FetchContent_Declare(
+    loguru
+    GIT_REPOSITORY https://github.com/emilk/loguru
+    GIT_SHALLOW 1
+    GIT_PROGRESS 1
+)
+
+if(NOT loguru_POPULATED)
+    FetchContent_Populate(loguru)
+    add_library(loguru INTERFACE)
+    target_include_directories(loguru INTERFACE ${loguru_SOURCE_DIR})
+    target_sources(loguru INTERFACE ${loguru_SOURCE_DIR}/loguru.cpp)
+endif()
