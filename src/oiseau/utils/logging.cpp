20788053af1f38f108c924253a7cdbb0fa7cfdc3
tiagovla <tiagovla@gmail.com>
2022-10-18 02:47:34 -0300

    fix: logging using cmake compile definitions

---
+++ src/oiseau/utils/logging.cpp
@@ -0,0 +1,9 @@
+#include "oiseau/utils/logging.hpp"
+#include <vector>
+
+void oiseau::logging::init(int argc, char** argv) {
+  loguru::g_stderr_verbosity = loguru::Verbosity_INFO;
+  std::vector<char*> argvv(argv, argv + argc);
+  argvv.push_back(nullptr);
+  loguru::init(argc, argvv.data());
+}
---
+++ src/oiseau/utils/logging.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "loguru.hpp"
+
+namespace oiseau::logging {
+void init(int argc, char** argv);
+}
