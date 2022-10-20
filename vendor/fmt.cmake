821d95462989b7aaaa4f2c9564578d37b261362f
tiagovla <tiagovla@gmail.com>
2022-10-20 19:57:56 -0300

    feat(vendor): add fmtlib

---
+++ vendor/fmt.cmake
@@ -0,0 +1,9 @@
+FetchContent_Declare(
+    fmt
+    GIT_REPOSITORY https://github.com/fmtlib/fmt
+    GIT_SHALLOW 1
+    GIT_PROGRESS 1
+    FIND_PACKAGE_ARGS REQUIRED
+)
+
+FetchContent_MakeAvailable(fmt)
