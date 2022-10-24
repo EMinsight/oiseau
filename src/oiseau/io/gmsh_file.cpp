19628c6fcedfff3b0f78e3070d42b5bdbc90693f
tiagovla <tiagovla@gmail.com>
2022-10-24 00:15:57 -0300

    fix: fix template order to match std::array

--- src/oiseau/io/gmsh_file.cpp
+++ src/oiseau/io/gmsh_file.cpp
@@ -31,7 +31,7 @@ const std::size_t gmsh_nodes_per_cell(const std::size_t s) {
 template <class T>
 concept Readable = std::is_convertible_v<T, std::string_view> || std::is_arithmetic_v<T>;
 
-template <int N, Readable T>
+template <Readable T, int N>
 std::array<T, N> from_file(std::istream& f, bool is_binary = false) {
   std::array<T, N> arr;
   if (is_binary) {
@@ -56,12 +56,12 @@ std::vector<T> from_file(std::istream& f, int n, bool is_binary = false) {
 }
 
 MeshFormatSection mesh_format_handler(std::istream& f_handler) {
-  auto [version] = from_file<1, double>(f_handler);
-  auto [is_binary] = from_file<1, int>(f_handler);
-  auto [size_t_size] = from_file<1, std::size_t>(f_handler);
+  auto [version] = from_file<double, 1>(f_handler);
+  auto [is_binary] = from_file<int, 1>(f_handler);
+  auto [size_t_size] = from_file<std::size_t, 1>(f_handler);
   if (is_binary) {
     f_handler.get();  // skip NF
-    auto [verify_one] = from_file<1, int>(f_handler, true);
+    auto [verify_one] = from_file<int, 1>(f_handler, true);
     if (verify_one != 1) throw std::runtime_error("Invalid GMSH file");
     if (size_t_size != sizeof(std::size_t)) throw std::runtime_error("Invalid GMSH file");
   }
@@ -69,7 +69,7 @@ MeshFormatSection mesh_format_handler(std::istream& f_handler) {
 };
 
 PhysicalNamesSection physical_names_handler(std::istream& f_handler) {
-  auto [num_phys_names] = from_file<1, int>(f_handler);
+  auto [num_phys_names] = from_file<int, 1>(f_handler);
   std::vector<int> dimensions;
   std::vector<int> physical_tags;
   std::vector<std::string> names;
@@ -77,8 +77,8 @@ PhysicalNamesSection physical_names_handler(std::istream& f_handler) {
   physical_tags.reserve(num_phys_names);
   names.reserve(num_phys_names);
   for (int i = 0; i < num_phys_names; i++) {
-    auto [dim, tag] = from_file<2, int>(f_handler);
-    auto [name] = from_file<1, std::string>(f_handler);
+    auto [dim, tag] = from_file<int, 2>(f_handler);
+    auto [name] = from_file<std::string, 1>(f_handler);
     dimensions.emplace_back(dim);
     physical_tags.emplace_back(tag);
     names.emplace_back(name);
@@ -87,17 +87,17 @@ PhysicalNamesSection physical_names_handler(std::istream& f_handler) {
 };
 
 EntitiesSection entities_handler(std::istream& f_handler, bool is_binary) {
-  auto quantity = from_file<4, std::size_t>(f_handler, is_binary);
+  auto quantity = from_file<std::size_t, 1>(f_handler, is_binary);
   std::array<std::vector<EntityEntry>, 4> blocks;
   for (std::size_t i = 0; i < 4; i++) blocks.at(i).reserve(quantity.at(i));
   for (int d = 0; d < 4; d++) {
     for (int j = 0; j < quantity[d]; j++) {
-      auto [tag] = from_file<1, int>(f_handler, is_binary);
+      auto [tag] = from_file<int, 1>(f_handler, is_binary);
       auto bounding_coods = from_file<double>(f_handler, (d == 0) ? 3 : 6, is_binary);
-      auto [num_physicals] = from_file<1, std::size_t>(f_handler, is_binary);
+      auto [num_physicals] = from_file<std::size_t, 1>(f_handler, is_binary);
       auto physical_tags = from_file<int>(f_handler, num_physicals, is_binary);
       if (d > 0) {
-        auto [num_BREP] = from_file<1, std::size_t>(f_handler, is_binary);
+        auto [num_BREP] = from_file<std::size_t, 1>(f_handler, is_binary);
         auto bounding_entities = from_file<int>(f_handler, num_BREP, is_binary);
         blocks.at(d).emplace_back(tag, std::move(bounding_coods), std::move(physical_tags),
                                   std::move(bounding_entities));
@@ -111,25 +111,25 @@ EntitiesSection entities_handler(std::istream& f_handler, bool is_binary) {
 
 NodesSection nodes_handler(std::istream& f_handler, bool is_binary) {
   auto [num_entity_blocks, total_num_nodes, min_node_tag, max_node_tag] =
-      from_file<4, std::size_t>(f_handler, is_binary);
+      from_file<std::size_t, 4>(f_handler, is_binary);
 
   std::vector<NodesBlock> blocks;
   blocks.reserve(num_entity_blocks);
 
   for (int i = 0; i < num_entity_blocks; i++) {
-    auto [dim, entity_tag, parametric] = from_file<3, int>(f_handler, is_binary);
-    auto [quantity] = from_file<1, std::size_t>(f_handler, is_binary);
+    auto [dim, entity_tag, parametric] = from_file<int, 3>(f_handler, is_binary);
+    auto [quantity] = from_file<std::size_t, 1>(f_handler, is_binary);
     std::vector<std::size_t> node_tags;
     std::vector<double> node_coords;
     node_tags.reserve(quantity);
     node_coords.reserve(quantity * 3);
 
     for (int j = 0; j < quantity; j++) {
-      auto [tag] = from_file<1, std::size_t>(f_handler, is_binary);
+      auto [tag] = from_file<std::size_t, 1>(f_handler, is_binary);
       node_tags.emplace_back(tag);
     }
     for (int j = 0; j < quantity; j++) {
-      auto xyz = from_file<3, double>(f_handler, is_binary);
+      auto xyz = from_file<double, 3>(f_handler, is_binary);
       node_coords.insert(node_coords.end(), xyz.begin(), xyz.end());
     }
     blocks.emplace_back(dim, entity_tag, parametric, quantity, std::move(node_tags),
@@ -140,12 +140,12 @@ NodesSection nodes_handler(std::istream& f_handler, bool is_binary) {
 
 ElementSection elements_handler(std::istream& f_handler, bool is_binary) {
   auto [num_element_blocks, num_elements, min_element_tag, max_element_tag] =
-      from_file<4, std::size_t>(f_handler, is_binary);
+      from_file<std::size_t, 4>(f_handler, is_binary);
   std::vector<ElementBlock> blocks;
   blocks.reserve(num_element_blocks);
   for (std::size_t i = 0; i < num_element_blocks; i++) {
-    auto [entity_dim, entity_tag, element_type] = from_file<3, int>(f_handler, is_binary);
-    auto [num_elements_in_block] = from_file<1, std::size_t>(f_handler, is_binary);
+    auto [entity_dim, entity_tag, element_type] = from_file<int, 3>(f_handler, is_binary);
+    auto [num_elements_in_block] = from_file<std::size_t, 1>(f_handler, is_binary);
     auto size = gmsh_nodes_per_cell(element_type);
     auto tmp_conn =
         from_file<std::size_t>(f_handler, (1 + size) * num_elements_in_block, is_binary);
