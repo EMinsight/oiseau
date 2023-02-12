9ce90d622ca35dd56aadd497adcb1a5458f210d9
tiagovla <tiagovla@gmail.com>
2023-02-12 19:26:54 -0300

    fix: change logging verbosity to warning

--- src/oiseau/utils/logging.cpp
+++ src/oiseau/utils/logging.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 void oiseau::logging::init(int argc, char** argv) {
-  loguru::g_stderr_verbosity = loguru::Verbosity_INFO;
+  loguru::g_stderr_verbosity = loguru::Verbosity_WARNING;
   std::vector<char*> argvv(argv, argv + argc);
   argvv.push_back(nullptr);
   loguru::init(argc, argvv.data());
