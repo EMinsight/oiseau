f1c6cdcf58b7ff17e95b90a29b0420b6a11d7921
tiagovla <tiagovla@gmail.com>
2023-02-13 22:57:29 -0300

    test: fix cell test

--- test/mesh/test_cell.cpp
+++ test/mesh/test_cell.cpp
@@ -4,6 +4,9 @@
 TEST(test_mesh, triangle_cell) {
   using namespace oiseau::mesh;
   auto tricell = TriangleCell();
-  EXPECT_STREQ(tricell.name().c_str(), "triangle");
+  EXPECT_STREQ(tricell.name().data(), "triangle");
+  EXPECT_STREQ(tricell.facet()->name().data(), "interval");
+  EXPECT_STREQ(tricell.edge()->name().data(), "point");
   EXPECT_EQ(tricell.dimension(), 2);
+  auto cell = TriangleCell();
 }
