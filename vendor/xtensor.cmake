d06f1505c2e2786174b4cf029c4106253f917ae6
tiagovla <tiagovla@gmail.com>
2025-04-30 02:01:14 -0300

    chore: set xtensor version

--- vendor/xtensor.cmake
+++ vendor/xtensor.cmake
@@ -1,6 +1,7 @@
 FetchContent_Declare(
     xtl
     GIT_REPOSITORY https://github.com/xtensor-stack/xtl.git
+    GIT_TAG 0.8.0
     GIT_SHALLOW 1
     GIT_PROGRESS 1
     FIND_PACKAGE_ARGS ""
@@ -9,6 +10,7 @@ FetchContent_Declare(
 FetchContent_Declare(
     xtensor
     GIT_REPOSITORY https://github.com/xtensor-stack/xtensor.git
+    GIT_TAG 0.26.0
     GIT_SHALLOW 1
     GIT_PROGRESS 1
     FIND_PACKAGE_ARGS ""
@@ -17,6 +19,7 @@ FetchContent_Declare(
 FetchContent_Declare(
     xtensor-blas
     GIT_REPOSITORY https://github.com/xtensor-stack/xtensor-blas
+    GIT_TAG 0.22.0
     GIT_SHALLOW 1
     GIT_PROGRESS 1
     FIND_PACKAGE_ARGS ""
