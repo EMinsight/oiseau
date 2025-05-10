4fff570daf78c5bbb0b896da49e5b2ff05425528
tiagovla <tiagovla@gmail.com>
2025-05-10 02:48:26 -0300

    refactor: optimize structure

--- src/oiseau/dg/nodal/ref_elements.cpp
+++ src/oiseau/dg/nodal/ref_elements.cpp
@@ -1,84 +1,74 @@
 #include "oiseau/dg/nodal/ref_elements.hpp"
+#include "oiseau/dg/nodal/utils.hpp"
+
+using namespace oiseau::dg::nodal::utils;
 
 namespace oiseau::dg::nodal {
-RefLine::RefLine(unsigned order) : m_order(order) {
-  m_r = oiseau::dg::nodal::utils::jacobi_gl(m_order, 0.0, 0.0);
-  m_np = order + 1;
-}
 
-xt::xarray<double> &RefLine::v() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = vandermonde_1d(m_order, m_r);
+RefLine::RefLine(unsigned order)
+    : m_order(order), m_r(jacobi_gl(order, 0.0, 0.0)), m_np(order + 1) {}
+
+xt::xarray<double>& RefLine::v() {
+  static xt::xarray<double> tmp = vandermonde_1d(m_order, m_r);
   return tmp;
 }
 
-xt::xarray<double> &RefLine::gv() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = grad_vandermonde_1d(m_order, m_r);
+xt::xarray<double>& RefLine::gv() {
+  static xt::xarray<double> tmp = grad_vandermonde_1d(m_order, m_r);
   return tmp;
 }
 
-xt::xarray<double> &RefLine::d() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = d_matrix_1d(v(), gv());
+xt::xarray<double>& RefLine::d() {
+  static xt::xarray<double> tmp = d_matrix_1d(v(), gv());
   return tmp;
 }
 
-xt::xarray<double> &RefLine::r() { return m_r; }
+xt::xarray<double>& RefLine::r() { return m_r; }
 
-RefTriangle::RefTriangle(unsigned order) : m_order(order) {
-  using namespace oiseau::dg::nodal::utils;
-  m_r = conversion_equilateral_xy_to_rs(generate_triangle_nodes(order));
-  m_np = m_order + 1;
-  m_nfp = m_order + 1;
-  m_np = ((m_order + 1) * (m_order + 2)) / 2;
-}
+RefTriangle::RefTriangle(unsigned order)
+    : m_order(order),
+      m_r(conversion_equilateral_xy_to_rs(generate_triangle_nodes(order))),
+      m_np(((order + 1) * (order + 2)) / 2),
+      m_nfp(order + 1) {}
 
-xt::xarray<double> &RefTriangle::v() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = vandermonde_2d(m_order, m_r);
+xt::xarray<double>& RefTriangle::v() {
+  static xt::xarray<double> tmp = vandermonde_2d(m_order, m_r);
   return tmp;
 }
 
-xt::xarray<double> &RefTriangle::gv() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = grad_vandermonde_2d(m_order, m_r);
+xt::xarray<double>& RefTriangle::gv() {
+  static xt::xarray<double> tmp = grad_vandermonde_2d(m_order, m_r);
   return tmp;
 }
 
-xt::xarray<double> &RefTriangle::d() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = d_matrix_2d(v(), gv());
+xt::xarray<double>& RefTriangle::d() {
+  static xt::xarray<double> tmp = d_matrix_2d(v(), gv());
   return tmp;
 }
 
-xt::xarray<double> &RefTriangle::r() { return m_r; }
+xt::xarray<double>& RefTriangle::r() { return m_r; }
 
-RefTetrahedron::RefTetrahedron(unsigned order) : m_order(order) {
-  using namespace oiseau::dg::nodal::utils;
-  m_r = conversion_equilateral_xyz_to_rst(generate_tetrahedron_nodes(order));
-  m_nfp = (m_order + 1) * (m_order + 2);
-  m_np = ((m_order + 1) * (m_order + 2) * (m_order + 3)) / 6;
-}
+RefTetrahedron::RefTetrahedron(unsigned order)
+    : m_order(order),
+      m_r(conversion_equilateral_xyz_to_rst(generate_tetrahedron_nodes(order))),
+      m_np(((order + 1) * (order + 2) * (order + 3)) / 6),
+      m_nfp((order + 1) * (order + 2)) {}
 
-xt::xarray<double> &RefTetrahedron::v() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = vandermonde_3d(m_order, m_r);
+xt::xarray<double>& RefTetrahedron::v() {
+  static xt::xarray<double> tmp = vandermonde_3d(m_order, m_r);
   return tmp;
 }
 
-xt::xarray<double> &RefTetrahedron::gv() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = grad_vandermonde_3d(m_order, m_r);
+xt::xarray<double>& RefTetrahedron::gv() {
+  static xt::xarray<double> tmp = grad_vandermonde_3d(m_order, m_r);
   return tmp;
 }
 
-xt::xarray<double> &RefTetrahedron::d() {
-  using namespace oiseau::dg::nodal::utils;
-  static auto tmp = d_matrix_3d(v(), gv());
+xt::xarray<double>& RefTetrahedron::d() {
+  static xt::xarray<double> tmp = d_matrix_3d(v(), gv());
   return tmp;
 }
 
-xt::xarray<double> &RefTetrahedron::r() { return m_r; }
+xt::xarray<double>& RefTetrahedron::r() { return m_r; }
 
 }  // namespace oiseau::dg::nodal
--- src/oiseau/dg/nodal/ref_elements.hpp
+++ src/oiseau/dg/nodal/ref_elements.hpp
@@ -1,88 +1,80 @@
 #pragma once
 
-#include <oiseau/dg/nodal/utils.hpp>
 #include <xtensor/containers/xarray.hpp>
 
 namespace oiseau::dg::nodal {
 
 class DGElement {
  public:
-  explicit DGElement() = default;
   virtual ~DGElement() = default;
 
-  virtual xt::xarray<double> &v() = 0;
-  virtual xt::xarray<double> &gv() = 0;
-  virtual xt::xarray<double> &d() = 0;
-  virtual xt::xarray<double> &r() = 0;
+  virtual xt::xarray<double>& v() = 0;
+  virtual xt::xarray<double>& gv() = 0;
+  virtual xt::xarray<double>& d() = 0;
+  virtual xt::xarray<double>& r() = 0;
 };
 
-class RefLine : DGElement {
- private:
-  const static unsigned int m_nfp = 1;
-  const static unsigned int m_nfaces = 2;
-
+class RefLine : public DGElement {
  public:
   explicit RefLine(unsigned order);
-  RefLine(const RefLine &) = default;
-  RefLine &operator=(RefLine &&) = default;
-  RefLine &operator=(const RefLine &) = default;
+  RefLine(const RefLine&) = default;
+  RefLine& operator=(RefLine&&) = default;
+  RefLine& operator=(const RefLine&) = default;
   ~RefLine() override = default;
 
-  xt::xarray<double> &v() override;
-  xt::xarray<double> &gv() override;
-  xt::xarray<double> &d() override;
-  xt::xarray<double> &r() override;
+  xt::xarray<double>& v() override;
+  xt::xarray<double>& gv() override;
+  xt::xarray<double>& d() override;
+  xt::xarray<double>& r() override;
 
  private:
-  xt::xarray<double> m_r;
+  static constexpr unsigned m_nfp = 1;
+  static constexpr unsigned m_nfaces = 2;
   unsigned m_order;
   unsigned m_np;
+  xt::xarray<double> m_r;
 };
 
-class RefTriangle : DGElement {
- private:
-  const static unsigned int m_nfaces = 3;
-
+class RefTriangle : public DGElement {
  public:
   explicit RefTriangle(unsigned order);
-  RefTriangle(const RefTriangle &) = default;
-  RefTriangle &operator=(RefTriangle &&) = default;
-  RefTriangle &operator=(const RefTriangle &) = default;
+  RefTriangle(const RefTriangle&) = default;
+  RefTriangle& operator=(RefTriangle&&) = default;
+  RefTriangle& operator=(const RefTriangle&) = default;
   ~RefTriangle() override = default;
 
-  xt::xarray<double> &v() override;
-  xt::xarray<double> &gv() override;
-  xt::xarray<double> &d() override;
-  xt::xarray<double> &r() override;
+  xt::xarray<double>& v() override;
+  xt::xarray<double>& gv() override;
+  xt::xarray<double>& d() override;
+  xt::xarray<double>& r() override;
 
  private:
-  xt::xarray<double> m_r;
+  static constexpr unsigned m_nfaces = 3;
   unsigned m_order;
   unsigned m_np;
-  unsigned int m_nfp = 1;
+  unsigned m_nfp;
+  xt::xarray<double> m_r;
 };
 
-class RefTetrahedron : DGElement {
- private:
-  const static unsigned int m_nfaces = 4;
-
+class RefTetrahedron : public DGElement {
  public:
   explicit RefTetrahedron(unsigned order);
-  RefTetrahedron(const RefTetrahedron &) = default;
-  RefTetrahedron &operator=(RefTetrahedron &&) = default;
-  RefTetrahedron &operator=(const RefTetrahedron &) = default;
+  RefTetrahedron(const RefTetrahedron&) = default;
+  RefTetrahedron& operator=(RefTetrahedron&&) = default;
+  RefTetrahedron& operator=(const RefTetrahedron&) = default;
   ~RefTetrahedron() override = default;
 
-  xt::xarray<double> &v() override;
-  xt::xarray<double> &gv() override;
-  xt::xarray<double> &d() override;
-  xt::xarray<double> &r() override;
+  xt::xarray<double>& v() override;
+  xt::xarray<double>& gv() override;
+  xt::xarray<double>& d() override;
+  xt::xarray<double>& r() override;
 
  private:
-  xt::xarray<double> m_r;
+  static constexpr unsigned m_nfaces = 4;
   unsigned m_order;
   unsigned m_np;
-  unsigned int m_nfp = 1;
+  unsigned m_nfp;
+  xt::xarray<double> m_r;
 };
 
 }  // namespace oiseau::dg::nodal
