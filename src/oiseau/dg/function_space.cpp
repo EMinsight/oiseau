05e43f810a9f421c75e77ecf67f23013708f631b
tiagovla <tiagovla@gmail.com>
2025-05-15 16:51:35 -0300

    feat: implement FunctionSpace class
    This commit introduces the `FunctionSpace` class, which manages the
    mesh, elements, and orders associated with a function space. It
    includes methods to access the elements and orders. The class
    uses shared pointers for the mesh to manage its lifecycle and
    spans to provide read-only access to the elements and orders.

--- src/oiseau/dg/function_space.cpp
+++ src/oiseau/dg/function_space.cpp
@@ -1,1 +1,19 @@
 #include "oiseau/dg/function_space.hpp"
+
+#include <cstddef>
+#include <memory>
+#include <span>
+#include <vector>
+
+#include "oiseau/dg/nodal/element.hpp"
+
+namespace oiseau::dg {
+
+FunctionSpace::FunctionSpace(const std::shared_ptr<const mesh::Mesh>& mesh,
+                             const std::vector<nodal::Element>& elements,
+                             const std::vector<std::size_t>& orders)
+    : m_mesh(mesh), m_elements(elements), m_orders(orders) {}
+std::span<const nodal::Element> FunctionSpace::elements() const { return {m_elements}; }
+std::span<const std::size_t> FunctionSpace::orders() const { return {m_orders}; }
+
+}  // namespace oiseau::dg
--- src/oiseau/dg/function_space.hpp
+++ src/oiseau/dg/function_space.hpp
@@ -1,23 +1,33 @@
 #pragma once
-#include <memory>
 
-namespace oiseau::mesh {
-class Mesh;
-}
+#include <cstddef>
+#include <memory>
+#include <span>
+#include <vector>
+
+#include "oiseau/dg/nodal/element.hpp"
+#include "oiseau/mesh/mesh.hpp"
 
 namespace oiseau::dg {
 
 class FunctionSpace {
  public:
   FunctionSpace(const FunctionSpace& V) = delete;
+  FunctionSpace(const std::shared_ptr<const mesh::Mesh>& mesh,
+                const std::vector<nodal::Element>& elements,
+                const std::vector<std::size_t>& orders);
   FunctionSpace(FunctionSpace&& V) = default;
   virtual ~FunctionSpace() = default;
   FunctionSpace& operator=(const FunctionSpace& V) = delete;
   FunctionSpace& operator=(FunctionSpace&& V) = default;
 
+  std::span<const nodal::Element> elements() const;
+  std::span<const std::size_t> orders() const;
+
  private:
   std::shared_ptr<const mesh::Mesh> m_mesh;
-  // std::shared_ptr<const mesh::Mesh> m_element; TODO: this might be a vector
+  std::vector<nodal::Element> m_elements;
+  std::vector<std::size_t> m_orders;
 };
 
 }  // namespace oiseau::dg
