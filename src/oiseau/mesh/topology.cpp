9c4973fa08a6ce73ecd559570081cd98ed724fd2
tiagovla <tiagovla@gmail.com>
2025-05-15 05:23:01 -0300

    refactor(topology): replace int loop variables with std::size_t for consistency and correctness in handling sizes and indices

--- src/oiseau/mesh/topology.cpp
+++ src/oiseau/mesh/topology.cpp
@@ -29,16 +29,16 @@ std::size_t Topology::n_cells() const { return m_conn.size(); }
 void Topology::calculate_connectivity() {
   // this should be extended to 3d and for mixed cells squares/triangles
   std::vector<std::vector<std::vector<std::size_t>>> faces;
-  for (int i = 0; i < m_conn.size(); i++) {
+  for (std::size_t i = 0; i < m_conn.size(); i++) {
     auto cell = m_cell_types[i];
     if (cell->kind() != CellKind::Triangle) continue;
     auto _conn = m_conn[i];
 
     auto face_vertices = cell->get_entity_vertices(1);
     std::vector<std::vector<std::size_t>> face(face_vertices.size());
-    for (int j = 0; j < face_vertices.size(); j++) {
+    for (std::size_t j = 0; j < face_vertices.size(); j++) {
       face[j].resize(face_vertices[j].size());
-      for (int k = 0; k < face_vertices[j].size(); k++) {
+      for (std::size_t k = 0; k < face_vertices[j].size(); k++) {
         face[j][k] = _conn[face_vertices[j][k]];
       }
       std::sort(face[j].begin(), face[j].end());
@@ -47,17 +47,17 @@ void Topology::calculate_connectivity() {
   }
   m_e_to_e.resize(faces.size());
   m_e_to_f.resize(faces.size());
-  for (int i = 0; i < m_e_to_e.size(); i++) {
+  for (std::size_t i = 0; i < m_e_to_e.size(); i++) {
     m_e_to_e[i] = std::vector<std::size_t>(faces[i].size(), i);
     m_e_to_f[i].resize(faces[i].size());
     std::iota(m_e_to_f[i].begin(), m_e_to_f[i].end(), 0);
   }
 
   // TODO: this is not efficient, should use a hash map
-  for (int i = 0; i < faces.size(); i++) {
-    for (int j = 0; j < faces[i].size(); j++) {
-      for (int ii = i + 1; ii < faces.size(); ii++) {
-        for (int jj = 0; jj < faces[ii].size(); jj++) {
+  for (std::size_t i = 0; i < faces.size(); i++) {
+    for (std::size_t j = 0; j < faces[i].size(); j++) {
+      for (std::size_t ii = i + 1; ii < faces.size(); ii++) {
+        for (std::size_t jj = 0; jj < faces[ii].size(); jj++) {
           if (m_e_to_e[i][j] != i || m_e_to_e[ii][jj] != ii) continue;
           if (faces[i][j] == faces[ii][jj]) {
             m_e_to_e[i][j] = ii;
