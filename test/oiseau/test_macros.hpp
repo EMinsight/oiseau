af8bf61d04ac12c7eede85a69983a2dc6e5cd860
tiagovla <tiagovla@gmail.com>
2025-05-17 03:54:41 -0300

    refactor(test_macros): improve EXPECT_FLOATS_NEARLY_EQ macro for better error reporting

--- test/oiseau/test_macros.hpp
+++ test/oiseau/test_macros.hpp
@@ -1,5 +1,13 @@
-#define EXPECT_FLOATS_NEARLY_EQ(expected, actual, thresh)                       \
-  EXPECT_EQ(expected.size(), actual.size()) << "Array sizes differ.";           \
-  for (size_t idx = 0; idx < std::min(expected.size(), actual.size()); ++idx) { \
-    EXPECT_NEAR(expected[idx], actual[idx], thresh) << "at index: " << idx;     \
+#define EXPECT_FLOATS_NEARLY_EQ(expected, actual, threshold)          \
+  ASSERT_EQ(expected.size(), actual.size()) << "Array sizes differ."; \
+  for (size_t idx = 0; idx < expected.size(); ++idx) {                \
+    auto diff = std::abs(expected[idx] - actual[idx]);                \
+    if (diff > threshold) {                                           \
+      FAIL() << "Stopped at first mismatch:\n"                        \
+             << "  index     : " << idx << "\n"                       \
+             << "  expected  : " << expected[idx] << "\n"             \
+             << "  actual    : " << actual[idx] << "\n"               \
+             << "  difference: " << diff << "\n"                      \
+             << "  threshold : " << threshold;                        \
+    }                                                                 \
   }
