14c5aa9be4225ba3d41866dd06ad9898052d9cd3
tiagovla <tiagovla@gmail.com>
2025-05-20 19:07:02 -0300

    build: improve cmake build system
    This commit introduces several improvements to the CMake build system:
    
    -   The vendor directory is now excluded from all builds by default.
    -   The oiseau library is now installed to the standard locations.
    -   The benchmark, demo, and test targets now link against oiseau_deps.
    -   The test include directories are now private.
    -   The loguru library now has the -fPIC compile option set.
    
    These changes improve the overall build process and make it easier to use the oiseau library.

--- test/macros.cmake
+++ test/macros.cmake
@@ -1,9 +1,9 @@
 macro(add_test test_name)
     add_executable(${test_name} ${ARGN})
-    target_link_libraries(${test_name} PRIVATE gtest gtest_main oiseau)
+    target_link_libraries(${test_name} PRIVATE gtest gtest_main oiseau oiseau_deps)
     set_target_properties(
         ${test_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin/test
     )
-    target_include_directories(${test_name} PUBLIC ${CMAKE_SOURCE_DIR}/test)
+    target_include_directories(${test_name} PRIVATE ${CMAKE_SOURCE_DIR}/test)
     gtest_discover_tests(${test_name})
 endmacro()
